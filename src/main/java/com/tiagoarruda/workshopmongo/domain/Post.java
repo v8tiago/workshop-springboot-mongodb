afd1e4bc2c3af8584c2f8eb7a1ab8e0e050457ad
Tiago Cruz <tiagoarrudadacruz@gmail.com>
2020-10-15 19:58:41 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/tiagoarruda/workshopmongo/domain/Post.java
+++ src/main/java/com/tiagoarruda/workshopmongo/domain/Post.java
@@ -1,11 +1,15 @@
 package com.tiagoarruda.workshopmongo.domain;
 
 import com.tiagoarruda.workshopmongo.dto.AuthorDTO;
+import com.tiagoarruda.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.xml.stream.events.Comment;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Document
 public class Post implements Serializable {
@@ -17,6 +21,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -67,6 +73,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
