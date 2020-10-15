56baf6e7569217c01a2bd0570f0983817a7d554f
Tiago Cruz <tiagoarrudadacruz@gmail.com>
2020-10-15 19:48:59 -0300

    Obtendo um post por id

---
+++ src/main/java/com/tiagoarruda/workshopmongo/resources/PostResource.java
@@ -0,0 +1,31 @@
+package com.tiagoarruda.workshopmongo.resources;
+
+import com.tiagoarruda.workshopmongo.domain.Post;
+import com.tiagoarruda.workshopmongo.domain.User;
+import com.tiagoarruda.workshopmongo.dto.UserDTO;
+import com.tiagoarruda.workshopmongo.services.PostService;
+import com.tiagoarruda.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value ="/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id)
+    {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
---
+++ src/main/java/com/tiagoarruda/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.tiagoarruda.workshopmongo.services;
+
+import com.tiagoarruda.workshopmongo.domain.Post;
+import com.tiagoarruda.workshopmongo.repository.PostRepository;
+import com.tiagoarruda.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id)
+    {
+        Optional<Post> post = repo.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
